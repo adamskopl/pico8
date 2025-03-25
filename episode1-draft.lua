2938dbc78ee979234f2a32f48d10d08f083e8c54
Adam Skobodziński <adam@skobo.pl>
2025-03-25 14:59:18 +0100

    random anvils

--- episode1-draft.lua
+++ episode1-draft.lua
@@ -8,7 +8,7 @@ end
 
 function _update()
   -- spawn anvils
-  if (#anvils < 5) then
+  if (#anvils < 5 and rnd(100) < 2) then
     add(anvils, {
       x = rnd(120),
       y = 0
@@ -17,7 +17,10 @@ function _update()
 
   -- move anvils
   for anvil in all(anvils) do
-    anvil.y = anvil.y + 1
+    anvil.y = anvil.y + 2
+    if (anvil.y > 128) then
+      del(anvils, anvil)
+    end
   end
 end
 
