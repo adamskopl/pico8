53cfa7a2c61961b80b88dbab202d089276d0076d
Adam Skobodziński <adam@skobo.pl>
2025-03-28 15:27:36 +0100

    game over

--- episode1-draft.lua
+++ episode1-draft.lua
@@ -2,6 +2,8 @@ function _init()
   player = {
     x = 128 / 2 - 8,
     y = 128 - 16,
+    w = 16,
+    h = 16,
     speed = 2,
     frame = 1,
     anim_dt = 0,
@@ -9,6 +11,7 @@ function _init()
   }
   anvils = {}
   score = 0
+  game_over = false
 end
 
 function _update()
@@ -29,7 +32,9 @@ function _update()
   if (#anvils < 5 and rnd(100) < 2) then
     add(anvils, {
       x = rnd(120),
-      y = 0
+      y = 0,
+      w = 8,
+      h = 8
     })
     score = score + 1
   end
@@ -40,6 +45,11 @@ function _update()
     if (anvil.y > 128) then
       del(anvils, anvil)
     end
+
+    -- collision
+    if (check_collision(player, anvil)) then
+      game_over = true
+    end
   end
 end
 
@@ -49,7 +59,7 @@ function _draw()
   for anvil in all(anvils) do
     spr(0, anvil.x, anvil.y, 1, 1)
   end
-  print("score: " .. score, 5, 5, 8)
+  print("score: " .. score .. ' game over: ' .. tostr(game_over), 5, 5, 8)
 end
 
 function animate_player()
@@ -60,4 +70,8 @@ function animate_player()
       player.frame = 1
     end
   end
+end
+
+function check_collision(a, b)
+  return (a.x < b.x + b.w) and (a.x + a.w > b.x) and (a.y < b.y + b.h) and (a.y + a.h > b.y)
 end
