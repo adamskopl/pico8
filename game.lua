9c378be860776eb03d4315a40263b09022c9566d
Adam Skobodziński <adam@skobo.pl>
2025-05-12 15:34:04 +0200

    u

--- game.lua
+++ game.lua
@@ -1,5 +1,8 @@
 function _init()
   printh("--init--")
+  cfg = {
+    mode = 1 -- 0=angles, 1=arrows
+  }
   p = {
     x = 1 * 8,
     y = 1 * 8,
@@ -10,28 +13,107 @@ function _init()
 end
 
 function _update60()
-  if btnp(0) then
-    cycle_target(1)
-  elseif btnp(1) then
-    cycle_target(-1)
-  elseif btn(2) then
-    move_towards(p, target)
-  elseif btn(3) then
+  if cfg.mode == 0 then
+    if btnp(0) then
+      cycle_target(1)
+    elseif btnp(1) then
+      cycle_target(-1)
+    elseif btn(2) then
+      move_towards(p, target)
+    elseif btn(3) then
+    end
+  elseif cfg.mode == 1 then
+    if btn(0) then
+      p.x = p.x - p.speed
+      aim(-1, 0)
+    elseif btn(1) then
+      p.x = p.x + p.speed
+      aim(1, 0)
+    elseif btn(2) then
+      p.y = p.y - p.speed
+      aim(0, -1)
+    elseif btn(3) then
+      p.y = p.y + p.speed
+      aim(0, 1)
+    end
+  end
+  if btnp(4) then
+    shoot()
+  end
+
+  for e in all(enemies) do
+    move_towards(e, p)
+  end
+
+  for b in all(bullets) do
+    local hit = move_towards(b, {
+      x = b.target.x + 3,
+      y = b.target.y + 3
+    })
+    if hit then
+      sfx(0)
+      del(bullets, b)
+      del(enemies, b.target)
+      target = nil
+    end
   end
 end
 
 function _draw()
   cls()
-  map(0, 0)
+  map(0, 0, 0, 0, 16, 16, 1)
   if target then
     rect(target.x, target.y, target.x + 8, target.y + 8, 12)
   end
   -- draw_lines_to_objs()
+  for e in all(enemies) do
+    spr(6, e.x, e.y)
+  end
+
   draw_p()
+  for b in all(bullets) do
+    pset(b.x, b.y, 8)
+  end
+
 end
 
 function draw_p()
   spr(1, p.x, p.y)
+end
+
+function normalize(x, y)
+  local len = sqrt(x * x + y * y)
+  if len == 0 then
+    return 0, 0
+  end
+  return x / len, y / len
+end
+
+function aim(dx, dy)
+  local closest = nil
+  local min_dist = 9999
+
+  for e in all(enemies) do
+    -- vector to enemy
+    local vx = e.x - p.x
+    local vy = e.y - p.y
+
+    -- normalize it
+    local nvx, nvy = normalize(vx, vy)
+
+    -- dot product with direction
+    local dot = dx * nvx + dy * nvy
+
+    if dot >= 0 then
+      local dist = vx * vx + vy * vy -- squared distance
+      if dist < min_dist then
+        min_dist = dist
+        closest = e
+      end
+    end
+  end
+
+  target = closest
 end
 
 function move_towards(o, target)
@@ -43,10 +125,12 @@ function move_towards(o, target)
   if distance < o.speed then
     o.x = target.x
     o.y = target.y
+    return true
   else
     o.x = o.x + dir_x * o.speed
     o.y = o.y + dir_y * o.speed
   end
+  return false
 end
 
 function draw_lines_to_objs()
@@ -71,10 +155,10 @@ function cycle_target(dir)
   local next_obj = nil
   local min_diff = 1 -- full circle
 
-  for obj in all(objs) do
+  for obj in all(enemies) do
     if obj ~= target then
       local angle_to = angle_between(p, obj)
-      local diff = angle_diff_cw(angle_from, angle_to, dir)
+      local diff = angle_diff_cw(angle_from, angle_to, 1)
       if diff < min_diff then
         min_diff = diff
         next_obj = obj
@@ -83,4 +167,17 @@ function cycle_target(dir)
   end
 
   target = next_obj
+end
+
+function shoot()
+  if not target then
+    return
+  end
+  local bullet = {
+    x = p.x + 3,
+    y = p.y + 3,
+    speed = 10,
+    target = target
+  }
+  add(bullets, bullet)
 end
--- map.lua
+++ map.lua
@@ -1,16 +1,21 @@
 function init_map()
   objs = {}
+  bullets = {}
+  enemies = {}
   for i = 0, 15 do
     for j = 0, 15 do
-      printh('DUPA')
-      printh(mget(i, j))
       local tile = mget(i, j)
       if tile == 2 or tile == 5 then
-        printh('add')
         add(objs, {
           x = i * 8,
+          y = j * 8
+        })
+      end
+      if tile == 6 then
+        add(enemies, {
+          x = i * 8,
           y = j * 8,
-          active = false
+          speed = 0.1
         })
       end
     end
